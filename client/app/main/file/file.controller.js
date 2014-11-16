6d145378af453a9e474705fe095caca3bda2fdca
pirhoo <pierre.romera@gmail.com>
2014-11-16 20:17:22 +0100

    Indent

--- client/app/main/file/file.controller.js
+++ client/app/main/file/file.controller.js
@@ -9,6 +9,7 @@ angular.module('fledit').controller('MainFileCtrl', function ($scope, $document,
   // Stringify received JSON to allow edition
   file.content = angular.toJson(file.content, true);
   $scope.file = file;
+
   // If a secret is given, we must store it and change search
   if($stateParams.secret) {
     $scope.secret = $stateParams.secret;
