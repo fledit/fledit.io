0b7676fcef47efaad302169698afecb2e43f6cce
Pirhoo <hello@pirhoo.com>
2014-11-22 23:57:39 +0100

    Added use contents

--- client/app/main/file/use/use.controller.js
+++ client/app/main/file/use/use.controller.js
@@ -1,4 +1,37 @@
 'use strict';
 
-angular.module('fledit').controller('MainFileUseCtrl', function ($scope, file, socket) {
+angular.module('fledit').controller('MainFileUseCtrl', function ($scope, $http, $location, file) {
+
+	$scope.setInfiniteLines = function(editor) {
+		editor.setOptions({
+		    maxLines: Infinity
+		});
+	}
+
+	$scope.jquery = [
+		"// Simply use the raw",
+		"var fileurl ='" + $scope.rawFilePath() + "';",
+		"// And get the data without unsing JSONP callback",
+		"$.getJSON(fileurl, function(data) {",
+		"    console.log(data);",
+		"});"
+	].join("\n")
+
+	$scope.socket = [
+		"// Create a socket ",
+		"var socket = io('ws://" + $location.host() + "', {path: '/socket.io-client'});",
+		"// Subscribe to this file",
+		"socket.emit('subscribe', '" + file._id +"');",
+		"// You'll now receive all updates",
+		"socket.on('save', function(file) {",
+		"    console.log(file);",
+		"});",
+	].join("\n")
+
+	$scope.embed = [
+		"<iframe  width=\"100%\" height=\"400\" ",
+		"src=\"" + $scope.publicFilePath() + "\" ",
+		"frameborder=\"0\" allowfullscreen>",
+		"</iframe>"
+	].join("")
 });
