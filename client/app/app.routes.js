887a183d87178f8d6bcd43f07e4a4d43801766fe
pirhoo <pierre.romera@gmail.com>
2014-12-30 19:13:43 +0100

    Debug ngAnnotate

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -4,10 +4,14 @@ angular.module('fledit').config(function ($urlRouterProvider, $stateProvider) {
   $urlRouterProvider.otherwise('/');
   // @src http://stackoverflow.com/questions/26824628/ui-router-state-current-wrapper-for-arbitary-state/26848546#26848546
   // This fn is called by StateBuilder each time a state is registered
-  $stateProvider.decorator('parent', function (internalStateObj, parentFn) {
+  var decoratorFn = function($state, parent) {
     // The first arg is the internal state. Capture it and add an accessor to public state object.
-    internalStateObj.self.$$state = function() { return internalStateObj; };
+    $state.self.$$state = function() { return $state; };
     // pass through to default .parent() function
-    return parentFn(internalStateObj);
-  });
+    return parent($state);
+  };
+  // We fake the DI to avoid a bug with ngAnnotate
+  // @src https://github.com/olov/ng-annotate/issues/82#issuecomment-63346833
+  decoratorFn.$inject = ['$state', 'parent'];
+  $stateProvider.decorator('views', decoratorFn);
 });
