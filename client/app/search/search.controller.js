3f160937ad638cd5d3ae3ef52cbdd7bc4a199649
pirhoo <pierre.romera@gmail.com>
2015-04-26 01:20:44 +0200

    Added search template

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('fledit')
-  .controller('SearchCtrl', function ($scope, $state, files, Paginator) {
+  .controller('SearchCtrl', function ($scope, $state, $stateParams, files, Paginator) {
     $scope.search = new Paginator(files);
-    $scope.search.next();
+    $scope.search.q = $stateParams.q;
+    // Submit a new search, go straight to the same state
+    $scope.submit = function(q) { $state.go('search', { q: q }); };
   });
