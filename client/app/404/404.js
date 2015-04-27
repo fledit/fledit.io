93e7c929503b3a9b2a4c004517f58d83c6f22e41
pirhoo <pierre.romera@gmail.com>
2015-04-26 21:32:24 -0400

    Updated 404

--- client/app/404/404.js
+++ client/app/404/404.js
@@ -6,4 +6,9 @@ angular.module('fledit')
       .state('404', {
         templateUrl: 'app/404/404.html'
       });
+    $stateProvider
+      .state('page-404', {
+        url: '/404',
+        templateUrl: 'app/404/404.html'
+      });
   });
