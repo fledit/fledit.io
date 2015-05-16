194544b33f79cfcac44bdaed43d0c1a8b414afbe
pirhoo <pierre.romera@gmail.com>
2015-05-16 15:23:36 +0200

    Copy data files too

--- Gruntfile.js
+++ Gruntfile.js
@@ -359,6 +359,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
+            'assets/data/**/*{json,js}',
             'index.html'
           ]
         }, {
