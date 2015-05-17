bc7c0cb36a9573f4533d50a8efb2aeabc118cf6e
pirhoo <pierre.romera@gmail.com>
2015-05-17 19:36:30 +0200

    Better form support

--- client/app/main/file/form/form.controller.js
+++ client/app/main/file/form/form.controller.js
@@ -3,6 +3,21 @@
 angular.module('fledit')
   .controller('MainFileFormCtrl', function ($scope, file, validator) {
     $scope.form = ["*"];
-    $scope.validator = validator;
+    // We may edit the validator
+    var validatorContent = angular.copy(validator.content);
+    // Hide the title
+    validatorContent.title = validatorContent.title ||  validator.name || file.name || 'Untitled';
+    // Ensure that the root item of the validator is an Object
+    validatorContent = {
+      type: 'object',
+      title: false,
+      properties: {
+        'neasted': validatorContent
+      }
+    };
+
+    $scope.validator = validatorContent;
     $scope.file = file;
+    // The form will edit a copy of the file object
+    $scope.fileBuffer = {'neasted': file.content };
   });
