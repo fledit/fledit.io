becc5ffadce3d853b3d7591e227adf61a068fa30
pirhoo <hello@pirhoo.com>
2015-10-25 12:07:08 +0100

    Add claim button

--- client/app/main/mine/mine.controller.js
+++ client/app/main/mine/mine.controller.js
@@ -3,11 +3,29 @@
 angular.module('fledit').controller('MainMineCtrl', function ($scope, filemanager, Restangular) {
   $scope.remove = function(file) {
     // Ask for confirmation
-    if( confirm("This file will be lost for you and every people who are using it. Are you sure?") ) {
+    if( confirm('This file will be lost for you and every people who are using it. Are you sure?') ) {
       // Take id and secret from state parameters
-      Restangular.one("files", file._id).remove({ secret: file.secret});
-      // Remove it from "my files"
-      filemanager.remove(file._id);
+      Restangular
+        .one('files', file._id)
+        .remove({ secret: file.secret})
+        .then(function() {
+          // Remove it from "my files"
+          filemanager.remove(file._id);          
+        })
     }
-  }
+  };
+
+  $scope.claim = function(file) {
+    // Take id and secret from state parameters
+    Restangular
+      .one('files', file._id)
+      .one('claim')
+      .patch({ secret: file.secret})
+      .then(function() {
+        // Remove it from "my files"
+        filemanager.remove(file._id);
+      });
+  };
+
+
 });
